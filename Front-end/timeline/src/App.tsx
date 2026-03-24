25822c1e7960ee6385bc4a38e95462de8a49c11d
BUR1T1 <otaviocaetano184@gmail.com>
2026-03-24 08:59:02 -0300

    Add Front

---
+++ Front-end/timeline/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Formulario from './componemts/FormularioLogin/Fromulario'
+import FormularioCadastro from './componemts/FormularioCadastro/FormularioCadastro'
+
+function App() {
+
+  return (
+      <FormularioCadastro />
+  )
+}
+
+export default App
